bdb5a9ed0791f7e68a8884856e1aabcaeb1c60eb
Sohail Saifi <sohailsaifi65@gmail.com>
2020-12-30 20:55:31 +0530

    activity date graph

--- app/graphs.py
+++ app/graphs.py
@@ -2,3 +2,16 @@ import plotly
 import plotly.graph_objs as go
 import plotly.express as px
 import json
+
+def activityDate_Graph(df):
+    fig_batch = {
+                    'data': [
+                        go.Scatter(
+                            x=df.index,
+                            y=df['Number of Messages'].values,
+                            # text=y,
+                            # textposition='auto',
+                            mode='lines'
+                        )],
+
+                   }
